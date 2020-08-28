d24fc0784acd76d166e1c3ca15a68c32fce3a94d
Priydarshi Singh <dryairship@gmail.com>
2020-08-28 21:05:07 +0530

    Send a message

--- config/config.js
+++ config/config.js
@@ -9,4 +9,5 @@ export default {
     newsAPIKey: process.env.NEWSAPI_KEY,
     messengerVerificationToken: process.env.MESSENGER_VERIFY_TOKEN,
     port: process.env.PORT,
+    graphAPIUrl: process.env.GRAPH_API_URL + "?access_token=" + process.env.PAGE_ACCESS_TOKEN
 };
--- routes/message.js
+++ routes/message.js
@@ -1,13 +1,51 @@
+import fetch from 'node-fetch';
+import config from '../config/config.js';
+
+var usersDict = {};
+
+function sendMessage(recipient, text){
+    let requestBody = {
+        'messaging_type': 'RESPONSE',
+        'recipient': {
+            'id': recipient
+        },
+        'message': {
+            'text': text
+        }
+    };
+    fetch(config.graphAPIUrl, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    })
+    .then(res => res.json())
+    .then(res => console.log(res));
+}
+
+function displayHelp(sender){
+    sendMessage(sender, "Helllllo");
+}
+
+function processMessage(sender, text){
+    if(!usersDict[sender]){
+        displayHelp(sender);
+        usersDict[sender] = true;
+    }
+}
 
 function verify(req, res){
     let body = req.body;
     if (body.object === 'page') {
         let entry = body.entry[0];
         let event = entry.messaging[0];
-        if(event.message && event.message.text){
-            console.log(event.message.text);
-        }
+        let sender = event.sender.id;
         res.status(200).send('EVENT_RECEIVED');
+        if(event.message && event.message.text){
+            processMessage(sender, event.message.text);
+        }
     } else {
         res.sendStatus(404);
     }
